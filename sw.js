5b9280591aeb4794e988f1eb91fc6e0bd66addd3
wzysgsg6 <wzysgsg6@users.noreply.github.com>
2026-09-18 14:43:20 +0800

    Clean old caches

--- sw.js
+++ sw.js
@@ -1,38 +1,4 @@
-const CACHE = 'today-class-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json',
-  './data.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => Promise.all(ASSETS.map((url) => cache.add(url).catch(() => null))))
-      .then(() => self.skipWaiting())
-  );
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  // 网络优先：在线时拿到最新课表；断网时回退到上次缓存。
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
-  );
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.registration.unregister()));
 });
